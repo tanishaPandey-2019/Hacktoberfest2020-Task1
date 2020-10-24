0478c5052cd6e13662296b150d8a514b79d32a37
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2020-10-24 21:53:29 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -6,20 +6,15 @@ int main()
     char country[20];
     char skill;
     int age;
-    
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%[^\n]*c",&name);
-    
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%s",&country);
-    
     printf("Enter your Age : ");//Get user Age
     scanf("%d",&age);
-    
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    
     printf("Thank you %s Happy Hacking",name);//Thank the user
 
     return 0;
